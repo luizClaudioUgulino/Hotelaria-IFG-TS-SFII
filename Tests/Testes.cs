b9d398fc994becbf77d8e02c66130b3a68e0777a
LCURC <luizclaudiougulino@gmail.com>
2023-06-17 12:34:27 -0300

    Adicionar arquivos de projeto.

---
+++ Tests/Testes.cs
@@ -0,0 +1,35 @@
+﻿using hotelaria;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+   
+
+    [TestClass]
+    public class Testes
+    {
+
+        [TestInitialize] public void Init() {
+        
+        
+        
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            int valor1 = 2;
+            int valor2 = 3;
+            int resultadoEsperado = 5;
+
+            // Act (Ação)
+            int resultado = 2;
+
+            // Assert (Verificação)
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+    }
+}
---
+++ hotelaria/PaginaLogin.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace hotelaria
+{
+    partial class PaginaLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.getNome = new System.Windows.Forms.TextBox();
+            this.getSenha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.Controls.Add(this.button1);
+            this.panelLogin.Controls.Add(this.label2);
+            this.panelLogin.Controls.Add(this.label1);
+            this.panelLogin.Controls.Add(this.getSenha);
+            this.panelLogin.Controls.Add(this.getNome);
+            this.panelLogin.Location = new System.Drawing.Point(62, 84);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(245, 187);
+            this.panelLogin.TabIndex = 0;
+            // 
+            // getNome
+            // 
+            this.getNome.Location = new System.Drawing.Point(26, 54);
+            this.getNome.Name = "getNome";
+            this.getNome.Size = new System.Drawing.Size(100, 20);
+            this.getNome.TabIndex = 1;
+            // 
+            // getSenha
+            // 
+            this.getSenha.Location = new System.Drawing.Point(26, 110);
+            this.getSenha.Name = "getSenha";
+            this.getSenha.Size = new System.Drawing.Size(100, 20);
+            this.getSenha.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Associado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // PaginaLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 411);
+            this.Controls.Add(this.panelLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PaginaLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.paginaLogin_Load);
+            this.panelLogin.ResumeLayout(false);
+            this.panelLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox getSenha;
+        private System.Windows.Forms.TextBox getNome;
+    }
+}
+
