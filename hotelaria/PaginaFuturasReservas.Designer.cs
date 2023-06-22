f8775a966de8044124b24c26bca2857a996fb670
LCURC <luizclaudiougulino@gmail.com>
2023-06-22 01:20:03 -0300

    Correção da tela de Pagina reservas futuras

--- hotelaria/PaginaFuturasReservas.Designer.cs
+++ hotelaria/PaginaFuturasReservas.Designer.cs
@@ -48,10 +48,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Tomato;
-            this.label2.Location = new System.Drawing.Point(1328, 32);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(996, 26);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(120, 29);
+            this.label2.Size = new System.Drawing.Size(93, 23);
             this.label2.TabIndex = 13;
             this.label2.Text = "RESERVAS";
             // 
@@ -60,29 +59,26 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Tomato;
-            this.label1.Location = new System.Drawing.Point(79, 62);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(59, 50);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(250, 29);
+            this.label1.Size = new System.Drawing.Size(195, 23);
             this.label1.TabIndex = 12;
             this.label1.Text = "RESERVAS PARA O DIA :";
             // 
             // getId
             // 
             this.getId.BackColor = System.Drawing.Color.Snow;
-            this.getId.Location = new System.Drawing.Point(80, 652);
-            this.getId.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.getId.Location = new System.Drawing.Point(60, 530);
             this.getId.Name = "getId";
-            this.getId.Size = new System.Drawing.Size(669, 22);
+            this.getId.Size = new System.Drawing.Size(503, 20);
             this.getId.TabIndex = 11;
             // 
             // bLimpar
             // 
             this.bLimpar.ForeColor = System.Drawing.Color.Tomato;
-            this.bLimpar.Location = new System.Drawing.Point(264, 684);
-            this.bLimpar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bLimpar.Location = new System.Drawing.Point(198, 556);
             this.bLimpar.Name = "bLimpar";
-            this.bLimpar.Size = new System.Drawing.Size(212, 28);
+            this.bLimpar.Size = new System.Drawing.Size(159, 23);
             this.bLimpar.TabIndex = 10;
             this.bLimpar.Text = "Limpar Seleção";
             this.bLimpar.UseVisualStyleBackColor = true;
@@ -91,10 +87,9 @@
             // bAtualizar
             // 
             this.bAtualizar.ForeColor = System.Drawing.Color.Tomato;
-            this.bAtualizar.Location = new System.Drawing.Point(484, 684);
-            this.bAtualizar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bAtualizar.Location = new System.Drawing.Point(363, 556);
             this.bAtualizar.Name = "bAtualizar";
-            this.bAtualizar.Size = new System.Drawing.Size(267, 28);
+            this.bAtualizar.Size = new System.Drawing.Size(200, 23);
             this.bAtualizar.TabIndex = 9;
             this.bAtualizar.Text = "Atualizar";
             this.bAtualizar.UseVisualStyleBackColor = true;
@@ -102,11 +97,11 @@
             // 
             // listView
             // 
+            this.listView.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listView.HideSelection = false;
-            this.listView.Location = new System.Drawing.Point(65, 94);
-            this.listView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listView.Location = new System.Drawing.Point(49, 76);
             this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(1333, 469);
+            this.listView.Size = new System.Drawing.Size(1001, 382);
             this.listView.TabIndex = 8;
             this.listView.UseCompatibleStateImageBehavior = false;
             // 
@@ -114,21 +109,21 @@
             // 
             this.getdia.BackColor = System.Drawing.Color.Tomato;
             this.getdia.ForeColor = System.Drawing.Color.Snow;
-            this.getdia.Location = new System.Drawing.Point(343, 65);
-            this.getdia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.getdia.Location = new System.Drawing.Point(257, 53);
             this.getdia.Name = "getdia";
-            this.getdia.Size = new System.Drawing.Size(63, 22);
+            this.getdia.Size = new System.Drawing.Size(48, 20);
             this.getdia.TabIndex = 15;
+            this.getdia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.getdia.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Tomato;
-            this.label3.Location = new System.Drawing.Point(80, 615);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(60, 500);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(347, 23);
+            this.label3.Size = new System.Drawing.Size(287, 18);
             this.label3.TabIndex = 16;
             this.label3.Text = "SELECIONE PELO ID DO RESERVA";
             // 
@@ -136,31 +131,32 @@
             // 
             this.getmes.BackColor = System.Drawing.Color.Tomato;
             this.getmes.ForeColor = System.Drawing.Color.Snow;
-            this.getmes.Location = new System.Drawing.Point(415, 65);
-            this.getmes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.getmes.Location = new System.Drawing.Point(311, 53);
             this.getmes.Name = "getmes";
-            this.getmes.Size = new System.Drawing.Size(63, 22);
+            this.getmes.Size = new System.Drawing.Size(48, 20);
             this.getmes.TabIndex = 17;
+            this.getmes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.getmes.Validating += new System.ComponentModel.CancelEventHandler(this.textBox3_Validating);
             // 
             // getano
             // 
             this.getano.BackColor = System.Drawing.Color.Tomato;
             this.getano.ForeColor = System.Drawing.Color.Snow;
-            this.getano.Location = new System.Drawing.Point(487, 65);
-            this.getano.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.getano.Location = new System.Drawing.Point(365, 53);
             this.getano.Name = "getano";
-            this.getano.Size = new System.Drawing.Size(88, 22);
+            this.getano.Size = new System.Drawing.Size(67, 20);
             this.getano.TabIndex = 18;
+            this.getano.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.getano.Validating += new System.ComponentModel.CancelEventHandler(this.textBox2_Validating);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Tomato;
-            this.label4.Location = new System.Drawing.Point(347, 42);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(260, 34);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(29, 20);
+            this.label4.Size = new System.Drawing.Size(22, 16);
             this.label4.TabIndex = 19;
             this.label4.Text = "Dia";
             // 
@@ -169,10 +165,9 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Tomato;
-            this.label5.Location = new System.Drawing.Point(415, 42);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(311, 34);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(34, 20);
+            this.label5.Size = new System.Drawing.Size(25, 16);
             this.label5.TabIndex = 20;
             this.label5.Text = "Mês";
             // 
@@ -181,18 +176,17 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Tomato;
-            this.label6.Location = new System.Drawing.Point(483, 39);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(362, 32);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(34, 20);
+            this.label6.Size = new System.Drawing.Size(26, 16);
             this.label6.TabIndex = 21;
             this.label6.Text = "Ano";
             // 
             // PaginaFuturasReservas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1531, 743);
+            this.ClientSize = new System.Drawing.Size(1148, 604);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -207,7 +201,6 @@
             this.Controls.Add(this.bAtualizar);
             this.Controls.Add(this.listView);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PaginaFuturasReservas";
             this.Text = "PaginaFuturasReservas";
             this.ResumeLayout(false);
--- hotelaria/PaginaFuturasReservas.cs
+++ hotelaria/PaginaFuturasReservas.cs
@@ -25,12 +25,12 @@ namespace hotelaria
             getdia.MaxLength = 2;
             getmes.MaxLength = 2;
 
-            listView.Columns.Add("id_reserva", 100, HorizontalAlignment.Left);
-            listView.Columns.Add("cpf", 100, HorizontalAlignment.Left);
+            listView.Columns.Add("ID reserva", 100, HorizontalAlignment.Left);
+            listView.Columns.Add("CPF", 100, HorizontalAlignment.Left);
             listView.Columns.Add("Entrada", 130, HorizontalAlignment.Left);
-            listView.Columns.Add("saida", 130, HorizontalAlignment.Left);
-            listView.Columns.Add("valor reserva", 100, HorizontalAlignment.Left);
-            listView.Columns.Add("numero quarto", 100, HorizontalAlignment.Left);
+            listView.Columns.Add("Saida", 130, HorizontalAlignment.Left);
+            listView.Columns.Add("Valor reserva", 150, HorizontalAlignment.Left);
+            listView.Columns.Add("Numero do quarto", 170, HorizontalAlignment.Left);
             CarregarDados();
         }
 
@@ -78,89 +78,124 @@ namespace hotelaria
         {
             String banco = "server=localhost;database=merceariareis;uid=root;pwd=012304";
             listView.Items.Clear();
-            if (Verficar() == true)
+            if (Verficar() == true && String.IsNullOrEmpty(getId.Text))
             {
 
                 String data = "" + getano.Text + "-" + getmes.Text + "-" + getdia.Text;
 
-                if (String.IsNullOrEmpty(getId.Text))
+                using (MySqlConnection connection = new MySqlConnection(banco))
                 {
+                    string query = "SELECT * FROM reservas WHERE DATE(data_entrada) = '" + data + "' and disponibilidade = 'aberto'";
+                    MySqlCommand command = new MySqlCommand(query, connection);
 
-                    using (MySqlConnection connection = new MySqlConnection(banco))
+                    try
                     {
-                        string query = "SELECT * FROM reservas WHERE DATE(data_entrada) = '" + data + "' and disponibilidade = 'aberto'";
-                        MySqlCommand command = new MySqlCommand(query, connection);
+                        connection.Open();
+                        MySqlDataReader reader = command.ExecuteReader();
 
-                        try
+                        while (reader.Read())
                         {
-                            connection.Open();
-                            MySqlDataReader reader = command.ExecuteReader();
 
-                            while (reader.Read())
-                            {
+                            ListViewItem item = new ListViewItem(reader["id_reserva"].ToString());
+                            item.SubItems.Add(reader["CPF_cliente"].ToString());
+                            item.SubItems.Add(reader["data_entrada"].ToString());
+                            item.SubItems.Add(reader["data_saida"].ToString());
+                            item.SubItems.Add(reader["valor_total_reserva"].ToString());
+                            item.SubItems.Add(reader["numero_quarto"].ToString());
 
-                                ListViewItem item = new ListViewItem(reader["id_reserva"].ToString());
-                                item.SubItems.Add(reader["CPF_cliente"].ToString());
-                                item.SubItems.Add(reader["data_entrada"].ToString());
-                                item.SubItems.Add(reader["data_saida"].ToString());
-                                item.SubItems.Add(reader["valor_total_reserva"].ToString());
-                                item.SubItems.Add(reader["numero_quarto"].ToString());
+                            listView.Items.Add(item);
 
-                                listView.Items.Add(item);
-
-                            }
-
-                            reader.Close();
                         }
-                        catch (Exception ex)
-                        {
-                            MessageBox.Show("Ocorreu um erro: " + ex.Message);
-                        }
+
+                        reader.Close();
                     }
-
-                }
-                else
-                {
-                    using (MySqlConnection connection = new MySqlConnection(banco))
+                    catch (Exception ex)
                     {
-                        string query = "SELECT * FROM reservas WHERE DATE(data_entrada) = '" + data + "' and disponibilidade = 'aberto' and CPF_cliente ='" + getId.Text + "'";
-                        MySqlCommand command = new MySqlCommand(query, connection);
-
-                        try
-                        {
-                            connection.Open();
-                            MySqlDataReader reader = command.ExecuteReader();
-
-                            while (reader.Read())
-                            {
-
-                                ListViewItem item = new ListViewItem(reader["id_reserva"].ToString());
-                                item.SubItems.Add(reader["CPF_cliente"].ToString());
-                                item.SubItems.Add(reader["data_entrada"].ToString());
-                                item.SubItems.Add(reader["data_saida"].ToString());
-                                item.SubItems.Add(reader["valor_total_reserva"].ToString());
-                                item.SubItems.Add(reader["numero_quarto"].ToString());
-
-                                listView.Items.Add(item);
-
-                            }
-
-                            reader.Close();
-                        }
-                        catch (Exception ex)
-                        {
-                            MessageBox.Show("Ocorreu um erro: " + ex.Message);
-                        }
+                        MessageBox.Show("Ocorreu um erro: " + ex.Message);
                     }
                 }
-
             }
-            else
+            else if (Verficar() == false && getId.Text != "")
             {
-                MessageBox.Show("Por favor preencher data");
-                CarregarDados();
+
+                String data = "" + getano.Text + "-" + getmes.Text + "-" + getdia.Text;
+
+                using (MySqlConnection connection = new MySqlConnection(banco))
+                {
+                    string query = "SELECT * FROM reservas WHERE disponibilidade = 'aberto' and id_reserva ='" + getId.Text + "'";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+
+                    try
+                    {
+                        connection.Open();
+                        MySqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+
+                            ListViewItem item = new ListViewItem(reader["id_reserva"].ToString());
+                            item.SubItems.Add(reader["CPF_cliente"].ToString());
+                            item.SubItems.Add(reader["data_entrada"].ToString());
+                            item.SubItems.Add(reader["data_saida"].ToString());
+                            item.SubItems.Add(reader["valor_total_reserva"].ToString());
+                            item.SubItems.Add(reader["numero_quarto"].ToString());
+
+                            listView.Items.Add(item);
+
+                        }
+
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ocorreu um erro: " + ex.Message);
+                    }
+                }
             }
+            else if (Verficar() == true && getId.Text != "")
+            {
+                String data = "" + getano.Text + "-" + getmes.Text + "-" + getdia.Text;
+
+                using (MySqlConnection connection = new MySqlConnection(banco))
+                {
+                    string query = "SELECT * FROM reservas WHERE DATE(data_entrada) = '" + data + "' and disponibilidade = 'aberto' and id_reserva ='" + getId.Text + "'";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+
+                    try
+                    {
+                        connection.Open();
+                        MySqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+
+                            ListViewItem item = new ListViewItem(reader["id_reserva"].ToString());
+                            item.SubItems.Add(reader["CPF_cliente"].ToString());
+                            item.SubItems.Add(reader["data_entrada"].ToString());
+                            item.SubItems.Add(reader["data_saida"].ToString());
+                            item.SubItems.Add(reader["valor_total_reserva"].ToString());
+                            item.SubItems.Add(reader["numero_quarto"].ToString());
+
+                            listView.Items.Add(item);
+
+                        }
+
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ocorreu um erro: " + ex.Message);
+                    }
+                }
+            }
+            else if (Verficar() == false && getId.Text != "")
+            {
+                CarregarDados();
+            }  
+
         }
+            
+        
         private Boolean Verficar()
         {
             if (String.IsNullOrEmpty(getdia.Text) || String.IsNullOrEmpty(getmes.Text)
@@ -184,5 +219,54 @@ namespace hotelaria
             getmes.Clear();
             getano.Clear();
         }
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verifica se a tecla pressionada é um número
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            // Verifica se o valor está fora do intervalo permitido
+            int valor;
+            if (!int.TryParse(textBox.Text, out valor) || valor < 1 || valor > 12)
+            {
+                textBox.Text = string.Empty; // Limpa o valor do TextBox
+                e.Cancel = true; // Cancela o evento de validação
+            }
+        }
+
+        private void textBox2_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            // Verifica se o valor está fora do intervalo permitido
+            int valor;
+            if (!int.TryParse(textBox.Text, out valor) || valor < 2020 || valor > 2026)
+            {
+                textBox.Text = string.Empty; // Limpa o valor do TextBox
+                e.Cancel = true; // Cancela o evento de validação
+            }
+        }
+        private void textBox3_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            // Verifica se o valor está fora do intervalo permitido
+            int valor;
+            if (!int.TryParse(textBox.Text, out valor) || valor < 1 || valor > 31)
+            {
+                textBox.Text = string.Empty; // Limpa o valor do TextBox
+                e.Cancel = true; // Cancela o evento de validação
+            }
+        }
+
     }
 }
